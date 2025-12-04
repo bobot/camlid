b33a93674b3737a0e5ba5a73f38a25e1f90017a5
François Bobot <francois.bobot@cea.fr>
2025-12-04 16:03:17 +0100

    Use C formatting macros working for 32/64 bit

--- tests/array/lib.c
+++ tests/array/lib.c
@@ -26,7 +26,7 @@ void f2(intptr_t *a, size_t len)
 {
     for (size_t i = 0; i < len; i++)
     {
-        printf("c:%li\n", a[i]);
+        printf("c:%" PRIiPTR "\n", a[i]);
         fflush(stdout);
         a[i] += 1;
     }
