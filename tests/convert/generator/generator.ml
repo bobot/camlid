c7e47273fffcd3871cc4edaa76dfd8a387d455f9
François Bobot <francois.bobot@cea.fr>
2025-12-09 16:52:28 +0100

    More explanation on an example

--- tests/convert/generator/generator.ml
+++ tests/convert/generator/generator.ml
@@ -6,9 +6,11 @@ let result =
     [ ("Data", [ ("data", int) ]); ("Error", [ ("error", int) ]) ]
 
 let data, data_or_status =
+  (* data_pc is a variable that will correspond to the C version of the parameter [data] in the stubbed function *)
   let data, data_pc =
     Expert.simple_param ~input:false ~output:false (ptr_ref int) ~name:"data"
   in
+  (* A conversion function takes normally two arguments a pointer to the destination here of type [result] and the source here [int.cty]. For this case we are adding the variable [data_pc] as an additional argument. All the functions that use the C function `combine_data_or_status` have automatically this additional argument added. In the stub function the variable [data_pc] is applied to this additional argument *)
   let ty =
     convert ~c_to_mlc:"combine_data_or_status" ~c:int.cty ~mlc:result
       ~using:[ data_pc ] ()
